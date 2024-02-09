934648ef17ea498f2c9c3bacfaa66eb5079eb08e
Matheus <matheus.resteves@gmail.com>
2024-02-09 16:59:22 -0300

    feat: some tests ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->menuBar->setStyleSheet("QMenuBar {color: white; }");
 }
 
 MainWindow::~MainWindow()
